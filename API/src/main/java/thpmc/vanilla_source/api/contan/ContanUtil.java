8ac34ba92ee2e2cfead91fd44da096276fe0bac8
BuildTools <unconfigured@null.spigotmc.org>
2022-05-31 01:06:00 +0900

    Add Contan support

--- API/src/main/java/thpmc/vanilla_source/api/contan/ContanUtil.java
+++ API/src/main/java/thpmc/vanilla_source/api/contan/ContanUtil.java
@@ -1,5 +1,7 @@
 package thpmc.vanilla_source.api.contan;
 
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.contan_lang.standard.functions.StandardFunctions;
 import org.contan_lang.syntax.tokens.Token;
 import thpmc.vanilla_source.api.contan.functions.Floor;
@@ -8,6 +10,10 @@ public class ContanUtil {
     
     public static void setUpContan() {
         StandardFunctions.FUNCTIONS.put("floor", new Floor(null, new Token(null, "floor", 5, null, null), null));
+    }
+
+    public static Location createLocation(World world, double x, double y, double z) {
+        return new Location(world, x, y, z);
     }
     
 }
